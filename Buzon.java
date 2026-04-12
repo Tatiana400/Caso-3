81f542d2dd5efab196f2e97c08f263384b6dc35a
Tatiana Martinez <t.martinezl@uniandes.edu.co>
2026-04-12 14:57:40 -0500

    Clase Buzon inicial

--- Buzon.java
+++ Buzon.java
@@ -1,1 +1,33 @@
 // PARA COMPARTIR LOS EVENTOS ENTRE THREADS
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Buzon {
+    private Queue<Evento> eventos;
+    private int capacidad;
+
+    public Buzon(int capacidad) {
+        this.capacidad = capacidad;
+        this.eventos = new LinkedList<>();
+    }
+
+    // REVISAR BIEN
+    public synchronized void enviar(Evento evento) throws InterruptedException {
+        while (eventos.size() >= capacidad) {
+            wait();
+        }
+        eventos.add(evento);
+        notifyAll();
+    }
+
+    // REVISAR BIEN
+    public synchronized Evento recibir() throws InterruptedException {
+        while (eventos.isEmpty()) {
+            wait();
+        }
+        Evento evento = eventos.poll();
+        notifyAll();
+        return evento;
+    }
+}
